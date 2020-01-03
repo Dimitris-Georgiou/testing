cfb546c8e5e562abc9aba0f83e46ca84fa242df7
Dimitris-Georgiou <dimitris.georgiou94@gmail.com>
2020-01-03 12:51:56 +0200

    sss

--- hello_world/main.py
+++ hello_world/main.py
@@ -10,13 +10,15 @@ def login_view():
         username = request.form["username"]
         passwd = request.form["passwd"]
 
-        if username == 'kitsios' and passwd == 'pass':
+        if username == 'kitsios' and passwd == 'password':
             msg = "Username and password are correct"
+            print(msg) 
             return render_template("index.html", message=msg)
         else:
             msg = "Username or password are incorrect"
+            print(msg) 
             return render_template("wrong.html", message=msg)
-        print(msg)    
+           
     return render_template("form.html", message=msg)
 
 
